9fc2018088002105e87b633d3b00d1a867f62627
Javier Rios <jrios@msi>
2015-08-11 07:30:05 +0200

    New branch with the Arduino Pro Mini

---
+++ TinySleep/MiniSleep.ino
@@ -0,0 +1,134 @@
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <EEPROM.h>
+#include <RTC.h>
+
+volatile uint8_t uart_usage = 0;
+
+const uint8_t EE_OSCCAL = 0x00;
+
+void setup() {
+  OSCCAL = EEPROM.read(EE_OSCCAL);
+  
+  pinMode(13,OUTPUT);
+  
+  ADCSRA = 0;             // disable ADC
+  power_all_disable ();   // Turn off modules
+  power_timer2_enable (); // Turn on timer2
+  power_twi_enable ();    // Turn on TWI
+  power_usart0_enable (); // Turn on Serial
+  
+  RTC.begin();
+  
+  // Interrupt on PD0 (RXD)
+  cli();
+  PCICR  |= (1 << PCIE2);    // set PCIE2 to enable PCMSK2 scan
+  PCMSK2 |= (1 << PCINT16);  // set PCINT16 to trigger an interrupt on state change 
+  sei();
+  
+  Serial.begin(9600);
+} 
+
+void loop() {
+  menu();
+  printRTC();
+  Serial.flush();
+  sleep();
+  if(uart_usage>0) {
+    uart_usage--;
+    if(uart_usage==0) {
+      PCMSK2 |= (1 << PCINT16);  // set PCINT16 to trigger an interrupt on PD0 (RXD) state change 
+    }
+  }
+}
+
+void menu() {
+  while(Serial.available()>0) {
+    uint8_t c = Serial.read();
+    switch(c) {
+      case 'h':
+        menu_help();
+        break;
+      case 's':
+        while(Serial.available()<19);
+        menu_set();
+        break;
+      case 'p':
+        printRTC();
+        break;
+      default:
+        Serial.print(F("Unknown command: "));
+        Serial.println(c);
+        break;
+    }
+  }
+}
+
+void menu_help() {
+  Serial.println(F(" MiniSleep v1.0"));
+  Serial.println();
+  Serial.println(F(" Commands:"));
+  Serial.println(F("   h - Show this help."));
+  Serial.println(F("   s - Set the rtc time."));
+  Serial.println(F("   p - Print the rtc time."));
+}
+
+uint16_t readN4() {
+  uint16_t n;
+  n = Serial.read()-'0';
+  n = n*10 + Serial.read()-'0';
+  n = n*10 + Serial.read()-'0';
+  n = n*10 + Serial.read()-'0';
+  return n;
+}
+
+uint8_t readN2() {
+  uint16_t n;
+  n = Serial.read()-'0';
+  n = n*10 + Serial.read()-'0';
+  return n;
+}
+
+void menu_set() {
+  uint16_t tmp16;
+  uint8_t  tmp8;
+  
+  RTC.yy = readN4()-2000;
+  Serial.read(); // Ignore '-'
+  RTC.mm = readN2();
+  Serial.read(); // Ignore '-'
+  RTC.dd = readN2();
+  Serial.read(); // Ignore ' '
+  RTC.hh = readN2();
+  Serial.read(); // Ignore ':'
+  RTC.mi = readN2();
+  Serial.read(); // Ignore ':'
+  RTC.ss = readN2();
+}
+
+void printRTC() {
+  char buf[32];
+  sprintf_P(buf,PSTR("%04d-%02d-%02d %02d:%02d:%02d"),2000+RTC.yy,RTC.mm,RTC.dd,RTC.hh,RTC.mi,RTC.ss);
+  Serial.println(buf);
+  Serial.println(RTC.millis());
+}
+
+void sleep() {
+  if(uart_usage) {
+    set_sleep_mode (SLEEP_MODE_IDLE);
+  } else {
+    set_sleep_mode (SLEEP_MODE_PWR_SAVE);
+  }
+  
+  cli();                // timed sequence follows
+  sleep_enable();
+  sleep_bod_disable();  // turn off brown-out in software
+  sei();                // guarantees next instruction executed
+  sleep_cpu ();         // sleep within 3 clock cycles of above
+}
+
+ISR(PCINT2_vect) {
+  PCMSK2 &= ~(1 << PCINT16);  // Disable interrupt on PD0 (RXD) state change 
+  uart_usage = 60;
+}
+
