93664488b29daa2a6a258c1bf5ca325b12e4bf52
fjriosp <jrios@jrios-pc.tedial.com>
2015-09-03 20:38:14 +0200

    test

--- MiniSleep/libraries/FastBit/FastBit.h
+++ MiniSleep/libraries/FastBit/FastBit.h
@@ -3,14 +3,34 @@
 
 #include <Arduino.h>
 
-static const uint8_t fBitMask8[] = {bit(0),bit(1),bit(2),bit(3),bit(4),bit(5),bit(6),bit(7)};
+#define CREATE_LOOKUP_TABLE(name , type,  ...)     \
+  const type PROGMEM name##_PGM[] = {__VA_ARGS__}; \
+  const type         name[]       = {__VA_ARGS__};
 
-// Redefine the bit macros
-#define bit(b)          (fBitMask8[b])
-#define bitRead(r,b)    (r  &  bit(b))
-#define bitSet(r,b)     (r |=  bit(b))
-#define bitClear(r,b)   (r &= ~bit(b))
-#define bitWrite(r,b,v) (v ? bitSet(r,b) : bitClear(r,b))
+#define READ_LOOKUP_TABLE(name,idx)   \
+  (__builtin_constant_p(idx)          \
+  ? (name[idx])                       \
+  : pgm_read_byte(name##_PGM+(idx)))
+
+// bit* macros
+CREATE_LOOKUP_TABLE(lt_fBit,uint8_t,_BV(0),_BV(1),_BV(2),_BV(3),_BV(4),_BV(5),_BV(6),_BV(7));
+#define fBit(b)          (READ_LOOKUP_TABLE(lt_fBit,b))
+#define fBitSet(r,b)     ((r) |=  fBit(b))
+#define fBitClear(r,b)   ((r) &= ~fBit(b))
+#define fBitRead(r,b)    ((r) &   fBit(b))
+#define fBitWrite(r,b,v) ((v) ? fBitSet(r,b) : fBitClear(r,b))
+
+// specific macros
+CREATE_LOOKUP_TABLE(lt_fPinToPORTX,uint16_t,
+  /*  0 */ (uint16_t)&PORTD,(uint16_t)&PORTD,(uint16_t)&PORTD,(uint16_t)&PORTD,(uint16_t)&PORTD,(uint16_t)&PORTD,(uint16_t)&PORTD,(uint16_t)&PORTD,
+  /*  8 */ (uint16_t)&PORTB,(uint16_t)&PORTB,(uint16_t)&PORTB,(uint16_t)&PORTB,(uint16_t)&PORTB,(uint16_t)&PORTB,
+  /* 14 */ (uint16_t)&PORTC,(uint16_t)&PORTC,(uint16_t)&PORTC,(uint16_t)&PORTC,(uint16_t)&PORTC,(uint16_t)&PORTC
+);
+
+
+#define fPinToPORTX(p) ((volatile uint8_t *)READ_LOOKUP_TABLE(lt_fPinToPORTX,p))
+#define fPinToDDRX(p)  (fPinToPORTX(p)+1)
+#define fPinToPINX(p)  (fPinToPORTX(p)+2)
 
 #endif
 
